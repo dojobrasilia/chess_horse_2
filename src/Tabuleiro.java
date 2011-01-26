1ea6405c801f7ed79f9f5c1da22e4d4f33faf009
Bruno Pedroso <Bruno@macasa.local>
2011-01-26 19:11:04 -0200

    Inicio do teste para ver se o cavalo nao saiu do tabuleiro borda superior esq

--- src/Tabuleiro.java
+++ src/Tabuleiro.java
@@ -2,12 +2,25 @@ import java.awt.Point;
 
 
 public class Tabuleiro {
+	
+	private Point ondeEstou;
 
 	public void visita(Point p) {
+		ondeEstou=p;
 	}
 
 	public Point[] anda(int i) {
-		return new Point[]{new Point(3,2)};
+		Point p[] = null;
+		if(ondeEstou.x-1>0 && ondeEstou.y-2 > 0)
+			p = new Point[] {new Point(ondeEstou.x-1,ondeEstou.y-2)};
+		else {
+			p = new Point[] {new Point(ondeEstou.x+1,ondeEstou.y+2)};
+		}
+		return p;
+	}
+
+	public Object getOndeEstou() {
+		return new Point(3,3);
 	}
 
 	
--- src/TabuleiroTest.java
+++ src/TabuleiroTest.java
@@ -20,10 +20,46 @@ public class TabuleiroTest {
 		assertEquals(1, caminho.length);
 		assert_movimento_valido(p1, caminho[0]);
 	}
+	
+	@Test
+	public void deve_andar_um_outro_movimento_valido(){
+		Tabuleiro t = new Tabuleiro();
+		Point p1 = new Point(3,3);
+		
+		t.visita(p1);
+		Point[] caminho = t.anda(1);
+		
+		assertEquals(1, caminho.length);
+		assert_movimento_valido(p1, caminho[0]);
+	}
+
+	@Test
+	public void nao_deve_sair_canto_0_0(){
+		Tabuleiro t = new Tabuleiro();
+		Point p1 = new Point(1,1);
+		
+		t.visita(p1);
+		Point[] caminho = t.anda(1);
+		
+		assertEquals(1, caminho.length);
+		assert_movimento_valido(p1, caminho[0]);
+	}
+	
+	@Test
+	public void deve_visitar_uma_casa_inicial(){
+		Tabuleiro t  = new Tabuleiro();
+		Point p1 = new Point(3,3);
+		t.visita(p1);
+		
+		assertEquals(p1,t.getOndeEstou());
+	}
+	
 
 	private void assert_movimento_valido(Point pi, Point pf) {
 		int mov_x = Math.abs(pf.x-pi.x);
 		int mov_y = Math.abs(pf.y-pi.y);
+		assertTrue(pf.x>0 && pf.x<9);
+		assertTrue(pf.y>0 && pf.y<9);
 		assertEquals(3, mov_x+mov_y );
 		assertNotSame(0, mov_x);
 		assertNotSame(0, mov_y);
